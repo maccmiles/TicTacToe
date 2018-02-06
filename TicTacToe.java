f02f6ba90949b0716fa05e29c2a2b851fbb3b20a
Miles Macchiaroli <macchiarolim@wit.edu>
2018-02-06 14:21:36 -0500

    TicTacToe
    Creation of code to run Game, Uses Board.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,7 @@
 /*
 / MacchiaroliM
 / CS2
-/ Jan 30 2018
+/ Feb 06 2018
 / Tic Tac Toe Main Class
 */
 import java.util.Scanner;
@@ -10,11 +10,54 @@ public class TicTacToe
 {
 	public static void main (String [] args)
 	{
-		Board b = new Board();//For Testing Purposes To Be Added
-		b.makeMove('X', 1, 0);//
-		b.makeMove('X', 2, 1);//
-		b.makeMove('X', 2, 2);//
-		System.out.println(b);//
-		System.out.println(b.gameWon());//
+		System.out.println("Tic - Tac - Toe");
+		System.out.println("Version 1.2");
+		System.out.println("================");
+		int row;
+		int col;
+		int turn = 0;
+		boolean won = false;
+		char piece = 'X';
+		Board b = new Board();//create Board
+		Scanner input = new Scanner(System.in);
+		do{
+			System.out.print("Please enter the desired Row for " + piece + " (1-3): ");
+			row = input.nextInt()-1;
+			System.out.print("Please enter the desired Column for " + piece + " (1-3): ");
+			col = input.nextInt()-1;
+			if((b.isValidMove(row, col) == true)&&(b.isEmpty(row, col))) {
+				b.makeMove(piece, row, col);
+				System.out.println(b);
+				if(piece == 'X') {
+					piece = 'O';
+				}else {
+					piece = 'X';
+				}
+				won = b.gameWon();
+				turn++;
+			}else if(b.isValidMove(row, col) == false) {
+				System.out.println("Move Not Valid.");
+			}else {
+				System.out.println("Selected space is already taken.");
+			}
+		}while((won == false)&&(turn<9));
+		if(won == true) {
+			if(piece == 'X') {
+				piece = 'O';
+			}else {
+				piece = 'X';
+			}
+			System.out.println("###################");
+			System.out.println("# CONGRATULATIONS #");
+			System.out.println("# Player: " + piece +"       #");
+			System.out.println("# Won in: " + turn-- +" turns #");
+			System.out.println("###################");
+		}else {
+			System.out.println("########################################");
+			System.out.println("# WINNER: NONE                         #");
+			System.out.println("# Strange Game,                        #");
+			System.out.println("# The only winning move is not to play #");
+			System.out.println("########################################");
+		}
 	}
 }
